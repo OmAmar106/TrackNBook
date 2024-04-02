64e3099e09c7329eadfd1468bae4fdac482ae060
OmAmar <bt23cse106@iiitn.ac.in>
2024-04-02 18:32:05 +0530

    added cities

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 #include<string.h>
+int N = 20;
+char locations[20][20] = {"Butibori","IIITN","Airport Metro"};
+int arr[10][10];
+void print(char arr[N][20]){
+    printf("\n");
+    for(int i=0;i<N;i++){
+        printf("%d. %s\n",i+1,arr[i]);
+    }
+}
 int main(){
     printf(" ________  _______        _       ______  _   __    _  _     ___     ______     _____      _____     _  __\n");
     printf("'   ||   '||      |     // \\   //       \\ || //      || \\     ||     ||     \\  //     \\   //     \\   || //\n");
@@ -10,6 +19,7 @@ int main(){
     printf("___________________________________________________________________________________________________________\n");
     //History check krne ke liye ek kam krna naam ke sath save krna , isse log in ka kuch toh use ajayega
     //make a array of string with places within nagpur and make a adjency matrix 
+    //set ditance and use arr as an adjacency matrix 
     while(1){
         printf("\n1. Sign Up\n");
         printf("2. Log In\n");
@@ -47,15 +57,42 @@ int main(){
             //check if these are in notepad or not
             int exist = 0;
             if(exist!=1){
-                int choice1;
-                printf("\nWelcome %s",username);
-                printf("\nWhat would you like to do today?");
-                printf("\n1. Book a Cab");
-                printf("\n2. Book a Bus");
-                printf("\n3. Shortest Path Calculator");
-                printf("\n4. Fare Calculator");
-                //shortest path calculator mai dijikistra algo lagane 
-                //aur options ke bade soche asap
+                int choice1 = 0;
+                while(choice1!=6){
+                    printf("\nWelcome %s",username);
+                    printf("\n\nWhat would you like to do today?");
+                    printf("\n1. Book a Cab");
+                    printf("\n2. Book a Bus");
+                    printf("\n3. Shortest Path Calculator");
+                    printf("\n4. Fare Calculator");
+                    printf("\n5. Veiw last Booking");
+                    printf("\n6. Log out");
+                    printf("\nEnter choice : ");
+                    scanf("%d",&choice1);
+                    //shortest path calculator mai dijikistra algo lagane 
+                    //aur options ke bade soche asap
+                    if(choice1==1){
+                        continue;
+                    }
+                    else if(choice1==4){
+                        int startpoint;
+                        print(locations);
+                        printf("\nEnter Start Point : ");
+                        scanf("%d",&startpoint);
+                        int endpoint;
+                        printf("\nEnter End Point : ");        
+                        scanf("%d",&endpoint);
+                        //print types of mode
+                        printf("\nEnter mode : ");
+                        //perform dijikistras algo then multiply depending upon the type of mode
+                    }
+                    else if(choice1==6){
+                        break;
+                    }
+                    else{
+                        printf("\nPlease enter from 1 to 6\n");
+                    }
+                }
             }
             else{
                 printf("\nWrong Username or Password please enter again.");
